3ae8fed3017529b15bf72e761acf40fe7896fddd
MihalGennadi4 <mihalgennadi4@gmail.com>
2022-04-05 12:48:50 +0300

    Исправлены комментарии

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -2,24 +2,25 @@ package com.tcs.edu.decorator;
 
 import java.time.Instant;
 
-/** Класс для изменениий принятых на вход сообщений.
- *
+/**
+ * Класс для изменениий принятых на вход сообщений.
  */
 public class TimestampMessageDecorator {
-    /** Перменная с кол-вом вызовов метода print
-     *
+    /**
+     * Перменная с кол-вом вызовов метода print
      */
-    public static int messageCount = 1; //Хотел чтоб по умолчанию было 0, но не получилось сделать правильный вывод в консоль
+    public static int messageCount = 1; //todo переделать счетчик
 
-    /** Метод добавляющий текущее время перед выводом сообщения принятого на вход.
+    /**
+     * Метод добавляющий текущее время перед выводом сообщения принятого на вход.
+     *
      * @param message сообщение принимаемое на вход.
      * @author m.petrukhin
      */
     public static String decorate(String message) {
-       final var decoratedMessage =" " + Instant.now() + " " + message;
+        final var decoratedMessage = " " + Instant.now() + " " + message;
         return decoratedMessage;
     }
-
 
 
 }
