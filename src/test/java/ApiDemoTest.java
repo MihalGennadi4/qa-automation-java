0e9417288477449d7db4b2dce7957e366471e09c
MihalGennadi4 <mihalgennadi4@gmail.com>
2022-06-13 22:49:44 +0300

    ДЗ 13.Х Изменена генерация стран. Сделана заготовка для улучшения удаления сим

--- src/test/java/ApiDemoTest.java
+++ src/test/java/ApiDemoTest.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,36 @@ import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.*;
 
 public class ApiDemoTest {
+
+    @DisplayName("Генерация рандомной страны")
+    public static String generateNewString() {
+        String newCountry = RandomStringUtils.randomAlphanumeric(2);
+        System.out.println(newCountry);
+        return newCountry;
+    }
+
+/*
+    @DisplayName("Получения ID новой страны")
+    public static JsonPath createNewCountryAndGetId() {
+        return given()
+                .body("{\n" +
+                        "  \"countryName\": \"" + ApiDemoTest.generateNewString() + "\"\n" +
+                        "}")
+                .when()
+                .post("/")
+                .then()
+                .extract()
+                .jsonPath();
+    }
+
+
+     public static String convertJsonToString(String string) {
+        JsonPath json = createNewCountryAndGetId();
+        String toReturn = json.get(string);
+        return toReturn;
+
+    }*/
+
     @DisplayName("Авторизация")
     @BeforeAll
     public static void setUpAuth() {
@@ -33,10 +64,11 @@ public class ApiDemoTest {
                 .statusCode(200)
                 .body(
                         "id", is(5),
-                        "countryName",is("Ac"),
+                        "countryName", is("Ac"),
                         "locations", not(empty())
                 );
     }
+
     @DisplayName("Поиск всех существующих записей")
     @Test
     public void shouldGetAllLocationsWhenPopulatedDb() {
@@ -45,9 +77,8 @@ public class ApiDemoTest {
                 .then()
                 .statusCode(200)
                 .body(
-                        "size()", is(11),
-                        "[5].countryName", is("Pl"),
-                        "[5].id", is(6),
+                        "[5].countryName", is("Fr"),
+                        "[5].id", is(8),
                         "[5].locations", not(empty())
 
                 );
@@ -59,7 +90,7 @@ public class ApiDemoTest {
         given()
                 .contentType("application/json")
                 .body("{\n" +
-                        "  \"countryName\": \"pp\"\n" +
+                        "  \"countryName\": \"" + ApiDemoTest.generateNewString() + "\"\n" +
                         "}")
                 .when()
                 .post("/api/countries")
@@ -74,7 +105,7 @@ public class ApiDemoTest {
         given()
                 .contentType("application/json")
                 .body("{\n" +
-                        "  \"countryName\": \"pp\"\n" +
+                        "  \"countryName\": \"" + ApiDemoTest.generateNewString() + "\"\n" +
                         "}")
                 .when()
                 .post("/api/countries")
@@ -82,29 +113,32 @@ public class ApiDemoTest {
                 .statusCode(500)
                 .body("countryName", not(empty()));
     }
+
     @DisplayName("Удаление известной страны")
     @Test
-    public void shouldDeleteCountryWhenCountryAlreadyExist(){
+    public void shouldDeleteCountryWhenCountryAlreadyExist() {
+        //    String id = convertJsonToString("id");
         int id =  (int) Math.round(Math.random() * 10 + 1);
         when()
-                .delete("/api/countries/{id}", id )
+                .delete("/api/countries/{id}", id)
                 .then()
                 .statusCode(204)
                 .header("x-appapp-params", not(empty()));
 
 
     }
+
     @DisplayName("Обновление CountryName через put")
     @Test
     public void shouldPutCountryNameWhenCountryAlreadyExist() {
-        int id =  (int) Math.round(Math.random() * 10 + 1);
+        int id = (int) Math.round(Math.random() * 10 + 1);
         given()
                 .contentType("application/json")
                 .body("{\n" +
-                        "  \"id\": "+id+ ",\n \"countryName\": \"Qq\"\n" +
-                        "}" )
+                        "  \"countryName\": \"" + ApiDemoTest.generateNewString() + "\"\n" +
+                        "}")
                 .when()
-                .put("/api/countries/{id}", id)
+                .put("/api/countries/1")
                 .then()
                 .statusCode(200)
                 .body("countryName", not(empty()));
@@ -113,18 +147,19 @@ public class ApiDemoTest {
     @DisplayName("Обновление CountryName через path")
     @Test
     public void shouldPathCountryWhenCountryAlreadyExist() {
-        int id =  (int) Math.round(Math.random() * 10 + 1);
+        int id = (int) Math.round(Math.random() * 10 + 1);
         given()
                 .contentType("application/json")
                 .body("{\n" +
-                        "  \"id\": "+id+ ",\n \"countryName\": \"Qq\"\n" +
-                        "}" )
+                        "  \"countryName\": \"" + ApiDemoTest.generateNewString() + "\"\n" +
+                        "}")
                 .when()
-                .patch("/api/countries/{id}", id)
+                .patch("/api/countries/1")
                 .then()
                 .statusCode(200)
                 .body("countryName", not(empty()));
     }
+/*
     @DisplayName("Счетчик считает")
     @Test
     public void shouldCounterWhenCountryArrayNotNull() {
@@ -134,5 +169,6 @@ public class ApiDemoTest {
                 .statusCode(200)
                 .body(is("10"));
     }
+*/
 
 }
